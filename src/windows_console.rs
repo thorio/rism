0ae10634d84527b6675b5e4cb32936bf714cedd7
thorou <thorou@chirality.de>
2024-02-24 13:40:26 +0100

    Fix CI

--- src/windows_console.rs
+++ src/windows_console.rs
@@ -5,7 +5,6 @@ use winapi::um::wincon::{AttachConsole, FreeConsole, ATTACH_PARENT_PROCESS};
 
 // Attempts to attach the parent processes console and fails silently if the parent has no console
 pub fn attach() {
-	log::trace!("attempting to attach console");
 	unsafe {
 		AttachConsole(ATTACH_PARENT_PROCESS);
 	}
@@ -13,7 +12,6 @@ pub fn attach() {
 
 // Attempts to detach the console and fails silently if the parent has no console
 pub fn detach() {
-	log::trace!("attempting to free console");
 	unsafe {
 		FreeConsole();
 	}
